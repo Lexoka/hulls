e8c0162ef09da4a864031a04fd76417a52648dd3
alexandre kouyoumdjian <kouyoumdjian@marius>
2017-04-10 13:25:54 +0200

    slices

--- hulls.py
+++ hulls.py
@@ -10,6 +10,25 @@ FREQUENCIES			= [1, 2, 4, 8, 13, 20, 30]
 SPEEDS				= [0.73, 1.46, 2.19]
 CONDITIONS			= list()
 
+"""
+	conditionTrials:
+		condition 0
+			trial 0
+				trial number				time		targetX	targetY
+				1 to 4 (per subject basis)	x.x sec		0 to 1000, in pixels
+			trial 1
+			trial 2
+			.
+			.
+			.
+			trial 12 (4 per subject, 3 subjects at the moment)
+		condition 1
+		condition 2
+		.
+		.
+		.
+		condition N
+"""
 
 def FillConditionList():
 	for angle in ANGLES:
@@ -22,33 +41,50 @@ def PrintList(ml):
 		print(elt)
 
 def LoadConditionTrials():
-    cdt = pickle.load( open("conditionTrials.p", "rb") ) # read binary
-    return cdt
+	cdt = pickle.load( open("conditionTrials.p", "rb") ) # read binary
+	return cdt
+
+def SliceTrial(trial, window):
+	slices = list()
+	currentSlice = list()
+	lastBeginning = trial[0,1]
+	for line in trial:
+		if line[1] - lastBeginning > window:
+			currentSlice = np.array(currentSlice)
+			slices.append(currentSlice)
+			currentSlice = list()
+			lastBeginning = line[1]
+		currentSlice.append(line)
+	return(slices)
+
+def PrintConditionTrial(conditionTrials):
+	ci = 1
+	for condition in conditionTrials:
+		print("Condition " + str(ci))
+		ct = 1
+		for trial in condition:
+			print("Trial " + str(ct))
+			PrintList(trial)
+			ct += 1
+		ci += 1
 
 def main():
-    FillConditionList()
-    #PrintList(CONDITIONS)
-    conditionTrials = LoadConditionTrials()
-    """
-    print(len(conditionTrials))
-    print("\n")
-    for i in range(10):
-        print(len(conditionTrials[i]))
-        print(len(conditionTrials[i][0]))
-        print(len(conditionTrials[i][0][0]))
-        print("\n")
+	FillConditionList()
+	#PrintList(CONDITIONS)
+	conditionTrials = LoadConditionTrials()
+	"""
+	res = SliceTrial(conditionTrials[0][0], 0.2)
+	print(len(res))
+	cs = 0
+	for myslice in res:
+		print("Slice " + str(cs))
+		PrintList(myslice)
+		cs += 1
+	"""
 
-    """
-    ci = 1
-    for condition in conditionTrials:
-        print("Condition " + str(ci))
-        ct = 1
-        for trial in condition:
-            print("Trial " + str(ct))
-            PrintList(trial)
-            ct += 1
-        ci += 1
-    """
+	
+
+	"""
 	points = np.random.rand(30, 2)
 	hull = ConvexHull(points)
 	#print(points)
